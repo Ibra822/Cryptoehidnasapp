1c854f2008d8f830e593f963fd9fd5ef740a8483
Ibra822 <164181121+Ibra822@users.noreply.github.com>
2024-03-20 20:41:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('copyButton').addEventListener('click', function() {
     navigator.clipboard.writeText('G6nfBpQqTtXYRwnacQkYieGp8ycdod1wEPcjDNkBXfyh');
-    alert('Address copied to clipboard!');
+    alert('Address copied!');
 });
