ee41e7766ba01042e5fb243167b73512aa428799
Ibra822 <164181121+Ibra822@users.noreply.github.com>
2024-03-21 12:55:47 +0300

    Update tokenomics.js

--- tokenomics.js
+++ tokenomics.js
@@ -1,1 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Заголовок и подключение стилей -->
+</head>
+<body class="tokenomics-background">
+    <!-- Шапка и навигация -->
+    <main>
+        <div class="text-box">
+            <!-- Информация о токеномике -->
+        </div>
+    </main>
+    <!-- Подвал сайта -->
+</body>
+</html>
 
