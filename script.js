13594afd6270817dbef1e2b3f6f1f2375d27a66b
Ibra822 <164181121+Ibra822@users.noreply.github.com>
2024-03-28 00:13:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ document.getElementById('copyButton').addEventListener('click', function() {
         console.error('Error in copying text: ', err);
     });
 });
+
+// Функция для переключения меню
+function toggleMenu() {
+    var navbar = document.querySelector('nav');
+    if (navbar.style.width === "100%" || navbar.style.width === "10%") {
+        navbar.style.width = "0"; // Скрыть меню
+    } else {
+        // Открыть меню
+        // Установить ширину в зависимости от размера экрана
+        if (window.innerWidth <= 768) {
+            navbar.style.width = "100%"; // Полная ширина для мобильных устройств
+        } else {
+            navbar.style.width = "10%"; // Частичная ширина для десктопов
+        }
+    }
+}
+
+// Добавляем слушатель для иконки меню
+document.querySelector('.menu-icon').addEventListener('click', toggleMenu);
