d79731906ec8b32afb886355ef709bf356848f2f
Ibra822 <164181121+Ibra822@users.noreply.github.com>
2024-03-29 01:11:20 +0300

    Update and rename script-about.js to about.js

---
+++ about.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    var navbar = document.getElementById("navbar");
+    navbar.classList.toggle("active");
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Добавляем слушатель событий для иконки гамбургера
+    document.querySelector('.menu-icon').addEventListener('click', toggleMenu);
+});
