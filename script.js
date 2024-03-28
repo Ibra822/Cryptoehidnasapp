73e20f339e7b8fff34aba0a88df8b5264e80fa44
Ibra822 <164181121+Ibra822@users.noreply.github.com>
2024-03-28 11:23:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,13 +9,22 @@ document.getElementById('copyButton').addEventListener('click', function() {
 // Функция для переключения меню
 function toggleMenu() {
     var navbar = document.getElementById("navbar");
-    if (navbar.style.left === "-100%") {
-        navbar.style.left = "0"; // Показать меню
-    } else {
-        navbar.style.left = "-100%"; // Скрыть меню
-    }
+    // Переключаем класс active для управления видимостью меню
+    navbar.classList.toggle("active");
 }
 
+// Установка слушателя событий при загрузке документа
 document.addEventListener('DOMContentLoaded', function() {
+    // Инициализируем изначальное скрытое состояние меню
+    var navbar = document.getElementById("navbar");
+    navbar.style.left = "-100%"; // Изначально скрываем меню
+
+    // Добавляем слушатель для иконки меню
     document.querySelector('.menu-icon').addEventListener('click', toggleMenu);
+
+    // Дополнительно проверяем, не активно ли меню (например, при перезагрузке страницы),
+    // и устанавливаем корректное начальное состояние
+    if (navbar.classList.contains("active")) {
+        navbar.style.left = "0"; // Если меню должно быть открыто
+    }
 });
