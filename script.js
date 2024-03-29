eb71176abb057f0a5af7fa610403aaa696437c76
Ibra822 <164181121+Ibra822@users.noreply.github.com>
2024-03-29 23:29:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
-document.getElementById('copyButton').addEventListener('click', function() {
-    navigator.clipboard.writeText('G6nfBpQqTtXYRwnacQkYieGp8ycdod1wEPcjDNkBXfyh').then(() => {
-        // Отображаем модальное окно
-        var modal = document.getElementById('copyModal');
-        modal.style.display = "block";
+// Эта функция отображает модальное окно
+function showModal() {
+    var modal = document.getElementById('copyModal');
+    modal.style.display = "block";
+}
 
-        // Скрываем модальное окно через 2 секунды
-        setTimeout(function(){
-            modal.style.display = "none";
-        }, 2000);
-    }).catch(err => {
+// Эта функция скрывает модальное окно
+function hideModal() {
+    var modal = document.getElementById('copyModal');
+    modal.style.display = "none";
+}
+
+// Функция для обработки копирования
+document.getElementById('copyButton').addEventListener('click', function() {
+    navigator.clipboard.writeText('G6nfBpQqTtXYRwnacQkYieGp8ycdod1wEPcjDNkBXfyh')
+    .then(() => {
+        showModal();
+        setTimeout(hideModal, 2000); // Скрываем модальное окно через 2 секунды
+    })
+    .catch(err => {
         console.error('Error in copying text: ', err);
     });
 });
 
 // Закрытие модального окна при клике на (x)
-var span = document.getElementsByClassName("close")[0];
-span.onclick = function() {
-    var modal = document.getElementById('copyModal');
-    modal.style.display = "none";
-}
+document.querySelector('.close').addEventListener('click', hideModal);
 
-// Закрытие модального окна при клике за его пределами
-window.onclick = function(event) {
+// Закрытие модального окна при клике вне его области
+window.addEventListener('click', function(event) {
     var modal = document.getElementById('copyModal');
     if (event.target === modal) {
-        modal.style.display = "none";
+        hideModal();
     }
-}
+});
 
 // Функция для переключения видимости меню
 function toggleMenu() {
