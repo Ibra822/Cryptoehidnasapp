e794794de4b6b80c40c406279e0249c39154c2f9
Ibra822 <164181121+Ibra822@users.noreply.github.com>
2024-03-29 22:38:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 document.getElementById('copyButton').addEventListener('click', function() {
-    navigator.clipboard.writeText('G6nfBpQqTtXYRwnacQkYieGp8ycdod1wEPcjDNkBXfyh')
-    .then(() => {
-        var modal = document.getElementById('myModal');
+    navigator.clipboard.writeText('G6nfBpQqTtXYRwnacQkYieGp8ycdod1wEPcjDNkBXfyh').then(() => {
+        // Отображаем модальное окно
+        var modal = document.getElementById('copyModal');
         modal.style.display = "block";
-        setTimeout(function(){ modal.style.display = "none"; }, 2000);
-    })
-    .catch(err => {
+
+        // Скрываем модальное окно через 2 секунды
+        setTimeout(function(){
+            modal.style.display = "none";
+        }, 2000);
+    }).catch(err => {
         console.error('Error in copying text: ', err);
     });
 });
 
-// Получаем элемент <span> который закрывает модальное окно
+// Закрытие модального окна при клике на (x)
 var span = document.getElementsByClassName("close")[0];
-
-// Когда пользователь кликает на <span> (x), закрываем модальное окно
 span.onclick = function() {
-    var modal = document.getElementById('myModal');
+    var modal = document.getElementById('copyModal');
     modal.style.display = "none";
 }
 
-// Когда пользователь кликает в любом месте за пределами модального окна, закрываем его
+// Закрытие модального окна при клике за его пределами
 window.onclick = function(event) {
-    var modal = document.getElementById('myModal');
+    var modal = document.getElementById('copyModal');
     if (event.target === modal) {
         modal.style.display = "none";
     }
