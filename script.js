4352fc95178f235da73ca8924ad43b99389997f0
Ibra822 <164181121+Ibra822@users.noreply.github.com>
2024-03-30 00:06:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-// Эта функция отображает модальное окно
+// Эта функция отображает модальное окно с анимацией
 function showModal() {
-    var modal = document.getElementById('copyModal');
-    modal.style.display = "block";
+  var modal = document.getElementById('copyModal');
+  modal.classList.add('show');
+  modal.classList.remove('hide');
 }
 
-// Эта функция скрывает модальное окно
+// Эта функция скрывает модальное окно с анимацией
 function hideModal() {
-    var modal = document.getElementById('copyModal');
-    modal.style.display = "none";
+  var modal = document.getElementById('copyModal');
+  modal.classList.add('hide');
+  setTimeout(() => {
+    modal.classList.remove('show', 'hide');
+    modal.style.display = "none"; // Это скроет модальное окно после завершения анимации исчезновения
+  }, 300); // Это значение должно соответствовать продолжительности вашей анимации исчезновения
 }
 
 // Функция для обработки копирования
 document.getElementById('copyButton').addEventListener('click', function() {
-    navigator.clipboard.writeText('G6nfBpQqTtXYRwnacQkYieGp8ycdod1wEPcjDNkBXfyh')
+  navigator.clipboard.writeText('G6nfBpQqTtXYRwnacQkYieGp8ycdod1wEPcjDNkBXfyh')
     .then(() => {
-        showModal();
-        setTimeout(hideModal, 2000); // Скрываем модальное окно через 2 секунды
+      showModal();
+      setTimeout(hideModal, 2000); // Скрываем модальное окно через 2 секунды
     })
     .catch(err => {
-        console.error('Error in copying text: ', err);
+      console.error('Error in copying text: ', err);
     });
 });
 
@@ -27,10 +32,10 @@ document.querySelector('.close').addEventListener('click', hideModal);
 
 // Закрытие модального окна при клике вне его области
 window.addEventListener('click', function(event) {
-    var modal = document.getElementById('copyModal');
-    if (event.target === modal) {
-        hideModal();
-    }
+  var modal = document.getElementById('copyModal');
+  if (event.target === modal) {
+    hideModal();
+  }
 });
 
 // Функция для переключения видимости меню
