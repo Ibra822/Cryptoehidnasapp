24a12b7889ed68e1773a8a619e71f25fbc3e30dd
Ibra822 <164181121+Ibra822@users.noreply.github.com>
2024-03-29 21:23:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 document.getElementById('copyButton').addEventListener('click', function() {
-    navigator.clipboard.writeText('G6nfBpQqTtXYRwnacQkYieGp8ycdod1wEPcjDNkBXfyh').then(() => {
-        alert('Address copied!');
+    navigator.clipboard.writeText('G6nfBpQqTtXYRwnacQkYieGp8ycdod1wEPcjDNkBXfyh')
+    .then(() => {
+        // Получаем элемент уведомления
+        var copyAlert = document.getElementById('copyAlert');
+        // Показываем уведомление
+        copyAlert.style.display = 'block';
+        // Скрываем уведомление через 2 секунды
+        setTimeout(function() {
+            copyAlert.style.display = 'none';
+        }, 2000);
     }).catch(err => {
-        console.error('Error in copying text: ', err);
-    });
-});
+        console.error('Error in copying text
 
 // Функция для переключения видимости меню
 function toggleMenu() {
