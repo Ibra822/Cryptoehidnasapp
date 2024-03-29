1080c440b114f513175aefd44bc3dba7fda5e6d2
Ibra822 <164181121+Ibra822@users.noreply.github.com>
2024-03-29 21:26:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
+// Получение элемента модального окна
+var modal = document.getElementById('myModal');
+// Получение элемента <span>, который закрывает модальное окно
+var span = document.getElementsByClassName("close")[0];
+
 document.getElementById('copyButton').addEventListener('click', function() {
-    navigator.clipboard.writeText('G6nfBpQqTtXYRwnacQkYieGp8ycdod1wEPcjDNkBXfyh')
-    .then(() => {
-        // Получаем элемент уведомления
-        var copyAlert = document.getElementById('copyAlert');
-        // Показываем уведомление
-        copyAlert.style.display = 'block';
-        // Скрываем уведомление через 2 секунды
-        setTimeout(function() {
-            copyAlert.style.display = 'none';
-        }, 2000);
+    navigator.clipboard.writeText('G6nfBpQqTtXYRwnacQkYieGp8ycdod1wEPcjDNkBXfyh').then(() => {
+        // Отображение модального окна
+        modal.style.display = "block";
+        // Установка таймера на автоматическое закрытие модального окна через 2 секунды
+        setTimeout(function(){ modal.style.display = "none"; }, 2000);
     }).catch(err => {
-        console.error('Error in copying text
+        console.error('Error in copying text: ', err);
+    });
+});
+
+// Когда пользователь нажимает на <span> (x), закрыть модальное окно
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Когда пользователь нажимает в любое место за пределами модального окна, закрыть его
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 // Функция для переключения видимости меню
 function toggleMenu() {
